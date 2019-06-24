a52dc5531b541470273f24cfa7a379b85fde1a1e
Sorumond <sorumond@gmail.com>
2019-06-24 13:40:28 +0300

    fixed logo in header

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './header.css';
 import {OrderDelivery} from "../order-delivery/order-delivery.jsx";
 import {PersonalAreaBar} from "../personal-area-bar/personal-area-bar";
+import logo from './image/logo.svg';
 
 export function Header() {
     return (<header className="header">
         <div className="header__wrapper">
             <div className="header__logo--wrapper">
-                <a className="header__logo" href="javascript: void(0)"/>
+                <a className="header__logo" href="javascript: void(0)">
+                </a>
             </div>
             <OrderDelivery/>
             <PersonalAreaBar/>
