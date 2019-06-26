cc284046a56cae25de12214b7d26bdea8c524ae8
Sorumond <sorumond@gmail.com>
2019-06-26 17:12:06 +0300

    working with second page

--- src/restaurant-preview/restaurant-preview.jsx
+++ src/restaurant-preview/restaurant-preview.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './restaurant-preview.css';
 
 export function RestaurantPreview() {
-    return (<div className="restaurant-preview">
-        <p className="restaurant-preview__name">Макдональдс</p>
-        <p className="restaurant-preview__about">₴₴ • Бургеры</p>
-        <p className="restaurant-preview__delivery-time">25-35 Min</p>
-    </div>)
+    return (<section className="restaurant-preview__background">
+        <div className="restaurant-preview__wrapper--big">
+            <div className="restaurant-preview restaurant-preview--big">
+                <span className="restaurant-preview__name restaurant-preview__name--big">Трактир «Пушкин»</span>
+                <span className="restaurant-preview__about restaurant-preview__about--big">₴₴₴ • Европейская</span>
+                <span
+                    className="restaurant-preview__delivery-time restaurant-preview__delivery-time--big">40-50 Min</span>
+            </div>
+        </div>
+    </section>)
 }
