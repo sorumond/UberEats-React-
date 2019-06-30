717b8fdae6fb53d90b0c2394c9314849d9bece11
Sorumond <sorumond@gmail.com>
2019-06-30 14:56:53 +0300

    #5 Remove unneeded modifier "-big"

--- src/components/Restaurant-preview/Restaurant-preview.jsx
+++ src/components/Restaurant-preview/Restaurant-preview.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Restaurant-preview_big.css';
+import './Restaurant-preview.css';
 
 export function RestaurantPreview() {
     return (<section className="Restaurant-preview__background">
-        <div className="Restaurant-preview__wrapper--big">
-            <div className="restaurant-preview Restaurant-preview--big">
-                <span className="Restaurant-preview__name Restaurant-preview__name--big">Трактир «Пушкин»</span>
-                <span className="Restaurant-preview__about Restaurant-preview__about--big">₴₴₴ • Европейская</span>
+        <div className="Restaurant-preview__wrapper">
+            <div className="restaurant-preview Restaurant-preview">
+                <span className="Restaurant-preview__name Restaurant-preview__name">Трактир «Пушкин»</span>
+                <span className="Restaurant-preview__about Restaurant-preview__about">₴₴₴ • Европейская</span>
                 <span
-                    className="Restaurant-preview__delivery-time Restaurant-preview__delivery-time--big">40-50 Min</span>
+                    className="Restaurant-preview__delivery-time Restaurant-preview__delivery-time">40-50 Min</span>
             </div>
         </div>
     </section>)
