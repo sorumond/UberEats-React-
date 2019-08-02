8117bf494f6d28bff6d1cf74b17ea5a5a98e4baa
Sorumond <sorumond@gmail.com>
2019-08-02 22:24:04 +0300

    remove usless buttons from header

--- src/components/Personal-area-bar/Personal-area-bar.jsx
+++ src/components/Personal-area-bar/Personal-area-bar.jsx
@@ -5,10 +5,10 @@ import basket from "./image/basket.svg";
 export function PersonalAreaBar(props) {
   return (
     <div className="Personal-area-bar">
-      <button className="Personal-area-bar__sign-in">Sign in</button>
-      <div className="Personal-area-bar__register--wrapper">
-        <button className="Personal-area-bar__register">Register</button>
-      </div>
+      {/*<button className="Personal-area-bar__sign-in">Sign in</button>*/}
+      {/*<div className="Personal-area-bar__register--wrapper">*/}
+      {/*  <button className="Personal-area-bar__register">Register</button>*/}
+      {/*</div>*/}
       <button
         className="Personal-area-bar__basket"
         onClick={() => {
